68d24669ec9a42d590522772cafb8cce23bc6402
WU <2943352614@qq.com>
2023-08-24 09:43:34 +0800

    去掉自环和快速选择数据集

--- src/data_loader.py
+++ src/data_loader.py
@@ -22,6 +22,12 @@ def normalize_features(features):
     return features
 
 
+def degree_matrix(adjacency_matrix):
+    # 计算邻接矩阵的度矩阵
+    degree_matrix = np.diag(np.sum(adjacency_matrix, axis=1))
+
+    return degree_matrix
+
 def sparse_to_tuple(sparse_matrix):
     if not sp.isspmatrix_coo(sparse_matrix):
         sparse_matrix = sparse_matrix.tocoo()
@@ -54,14 +60,14 @@ def load_data(dataset):
     names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
     objects = []
     for i in range(len(names)):
-        with open('../data/{}/ind.{}.{}'.format(dataset, dataset, names[i]), 'rb') as f:
+        with open('./Code/GCN-LPAgit/data/{}/ind.{}.{}'.format(dataset, dataset, names[i]), 'rb') as f:
             if sys.version_info > (3, 0):
                 objects.append(pkl.load(f, encoding='latin1'))
             else:
                 objects.append(pkl.load(f))
 
     x, y, tx, ty, allx, ally, graph = tuple(objects)
-    test_idx_reorder = parse_index_file("../data/{}/ind.{}.test.index".format(dataset, dataset))
+    test_idx_reorder = parse_index_file("./Code/GCN-LPAgit/data/{}/ind.{}.test.index".format(dataset, dataset))
     test_idx_range = np.sort(test_idx_reorder)
 
     if dataset == 'citeseer':
@@ -84,6 +90,33 @@ def load_data(dataset):
     graph = nx.from_dict_of_lists(graph)
     graph.add_edges_from([(i, i) for i in range(len(graph.nodes)) if not graph.has_edge(i, i)])  # add self-loops
     adj = nx.adjacency_matrix(graph)
+    '''
+    #print(adj)
+    adj_n = adj.toarray()
+    # print("邻接矩阵的形状:" ,adj.shape,adj_n.shape) 邻接矩阵的形状: (2708, 2708) (2708, 2708)
+    # print ("初始邻接矩阵元素个数：",adj.nnz , np.count_nonzero(adj_n))  初始邻接矩阵元素个数： 13264 13264
+    
+    degree = np.asarray(adj.sum(axis=1)).flatten()
+    degree_matrix = np.diag(degree)
+    
+    #print(degree_matrix )
+    
+    #print(type(degree_matrix),adj.shape, np.count_nonzero(adj_n),sum)
+    
+    adj2_n = np.matmul(adj_n, adj_n)
+    
+    #print ("二跳矩阵元素个数：",np.count_nonzero(adj2_n),sum2)
+    adj2 = adj.dot(adj)  #矩阵相乘
+    print (adj2,"二跳矩阵元素个数：",adj2.nnz)
+    adj2 = adj2 - degree  #对角线置0 二跳邻接矩阵 这一步将稀疏矩阵转为了常规矩阵
+    
+    
+    adj3 = adj.multiply(adj2) #按位乘法、三角结构矩阵
+    print (adj3,"三角结构矩阵元素个数：",adj3.nnz)
+    adj = adj + adj3 #三角结构矩阵强化邻接矩阵
+    adj = sp.csr_matrix(adj) 
+    print (adj,"改进邻接矩阵元素个数：",adj.nnz)
+    '''
     adj = sparse_to_tuple(adj)
 
     train_mask, val_mask, test_mask = split_dataset(len(graph.nodes))
--- src/main.py
+++ src/main.py
@@ -5,88 +5,74 @@ from time import time
 from data_loader import load_data, load_npz, load_random
 from train import train
 
+
+def switch_case(argument):
+    if argument == 1:
+        # 执行操作1:cora
+        parser.add_argument('--dataset', type=str, default='cora', help='which dataset to use')
+        parser.add_argument('--epochs', type=int, default=200, help='the number of epochs')
+        parser.add_argument('--dim', type=int, default=32, help='dimension of hidden layers')
+        parser.add_argument('--gcn_layer', type=int, default=5, help='number of GCN layers')
+        parser.add_argument('--lpa_iter', type=int, default=5, help='number of LPA iterations')
+        parser.add_argument('--l2_weight', type=float, default=1e-4, help='weight of l2 regularization')
+        parser.add_argument('--lpa_weight', type=float, default=10, help='weight of LP regularization')
+        parser.add_argument('--dropout', type=float, default=0.2, help='dropout rate')
+        parser.add_argument('--lr', type=float, default=0.05, help='learning rate')
+    elif argument == 2:
+        # 执行操作2:citeseer
+        parser.add_argument('--dataset', type=str, default='citeseer', help='which dataset to use')
+        parser.add_argument('--epochs', type=int, default=200, help='the number of epochs')
+        parser.add_argument('--dim', type=int, default=16, help='dimension of hidden layers')
+        parser.add_argument('--gcn_layer', type=int, default=2, help='number of GCN layers')
+        parser.add_argument('--lpa_iter', type=int, default=5, help='number of LPA iterations')
+        parser.add_argument('--l2_weight', type=float, default=5e-4, help='weight of l2 regularization')
+        parser.add_argument('--lpa_weight', type=float, default=1, help='weight of LP regularization')
+        parser.add_argument('--dropout', type=float, default=0, help='dropout rate')
+        parser.add_argument('--lr', type=float, default=0.2, help='learning rate')
+    elif argument == 3:
+        # 执行操作3:pubmed
+        parser.add_argument('--dataset', type=str, default='pubmed', help='which dataset to use')
+        parser.add_argument('--epochs', type=int, default=200, help='the number of epochs')
+        parser.add_argument('--dim', type=int, default=32, help='dimension of hidden layers')
+        parser.add_argument('--gcn_layer', type=int, default=2, help='number of GCN layers')
+        parser.add_argument('--lpa_iter', type=int, default=1, help='number of LPA iterations')
+        parser.add_argument('--l2_weight', type=float, default=2e-4, help='weight of l2 regularization')
+        parser.add_argument('--lpa_weight', type=float, default=1, help='weight of LP regularization')
+        parser.add_argument('--dropout', type=float, default=0, help='dropout rate')
+        parser.add_argument('--lr', type=float, default=0.1, help='learning rate')
+    elif argument == 4:
+        # 执行操作4:coauthor-cs
+        parser.add_argument('--dataset', type=str, default='coauthor-cs', help='which dataset to use')
+        parser.add_argument('--epochs', type=int, default=200, help='the number of epochs')
+        parser.add_argument('--dim', type=int, default=32, help='dimension of hidden layers')
+        parser.add_argument('--gcn_layer', type=int, default=2, help='number of GCN layers')
+        parser.add_argument('--lpa_iter', type=int, default=2, help='number of LPA iterations')
+        parser.add_argument('--l2_weight', type=float, default=1e-4, help='weight of l2 regularization')
+        parser.add_argument('--lpa_weight', type=float, default=2, help='weight of LP regularization')
+        parser.add_argument('--dropout', type=float, default=0.2, help='dropout rate')
+        parser.add_argument('--lr', type=float, default=0.1, help='learning rate')
+    elif argument == 5:
+        # 执行操作5:coauthor-phy
+        parser.add_argument('--dataset', type=str, default='coauthor-phy', help='which dataset to use')
+        parser.add_argument('--epochs', type=int, default=200, help='the number of epochs')
+        parser.add_argument('--dim', type=int, default=32, help='dimension of hidden layers')
+        parser.add_argument('--gcn_layer', type=int, default=2, help='number of GCN layers')
+        parser.add_argument('--lpa_iter', type=int, default=3, help='number of LPA iterations')
+        parser.add_argument('--l2_weight', type=float, default=1e-4, help='weight of l2 regularization')
+        parser.add_argument('--lpa_weight', type=float, default=1, help='weight of LP regularization')
+        parser.add_argument('--dropout', type=float, default=0.2, help='dropout rate')
+        parser.add_argument('--lr', type=float, default=0.05, help='learning rate')
+    else:
+        # 无效的选择
+        print("无效的数据集选择")
+
 seed = 234
 np.random.seed(seed)
 tf.set_random_seed(seed)
 
 parser = argparse.ArgumentParser()
 
-'''
-# cora
-parser.add_argument('--dataset', type=str, default='cora', help='which dataset to use')
-parser.add_argument('--epochs', type=int, default=200, help='the number of epochs')
-parser.add_argument('--dim', type=int, default=32, help='dimension of hidden layers')
-parser.add_argument('--gcn_layer', type=int, default=5, help='number of GCN layers')
-parser.add_argument('--lpa_iter', type=int, default=5, help='number of LPA iterations')
-parser.add_argument('--l2_weight', type=float, default=1e-4, help='weight of l2 regularization')
-parser.add_argument('--lpa_weight', type=float, default=10, help='weight of LP regularization')
-parser.add_argument('--dropout', type=float, default=0.2, help='dropout rate')
-parser.add_argument('--lr', type=float, default=0.05, help='learning rate')
-'''
-
-# citeseer
-parser.add_argument('--dataset', type=str, default='citeseer', help='which dataset to use')
-parser.add_argument('--epochs', type=int, default=200, help='the number of epochs')
-parser.add_argument('--dim', type=int, default=16, help='dimension of hidden layers')
-parser.add_argument('--gcn_layer', type=int, default=2, help='number of GCN layers')
-parser.add_argument('--lpa_iter', type=int, default=5, help='number of LPA iterations')
-parser.add_argument('--l2_weight', type=float, default=5e-4, help='weight of l2 regularization')
-parser.add_argument('--lpa_weight', type=float, default=1, help='weight of LP regularization')
-parser.add_argument('--dropout', type=float, default=0, help='dropout rate')
-parser.add_argument('--lr', type=float, default=0.2, help='learning rate')
-
-'''
-# pubmed
-parser.add_argument('--dataset', type=str, default='pubmed', help='which dataset to use')
-parser.add_argument('--epochs', type=int, default=200, help='the number of epochs')
-parser.add_argument('--dim', type=int, default=32, help='dimension of hidden layers')
-parser.add_argument('--gcn_layer', type=int, default=2, help='number of GCN layers')
-parser.add_argument('--lpa_iter', type=int, default=1, help='number of LPA iterations')
-parser.add_argument('--l2_weight', type=float, default=2e-4, help='weight of l2 regularization')
-parser.add_argument('--lpa_weight', type=float, default=1, help='weight of LP regularization')
-parser.add_argument('--dropout', type=float, default=0, help='dropout rate')
-parser.add_argument('--lr', type=float, default=0.1, help='learning rate')
-'''
-
-'''
-# coauthor-cs
-parser.add_argument('--dataset', type=str, default='coauthor-cs', help='which dataset to use')
-parser.add_argument('--epochs', type=int, default=200, help='the number of epochs')
-parser.add_argument('--dim', type=int, default=32, help='dimension of hidden layers')
-parser.add_argument('--gcn_layer', type=int, default=2, help='number of GCN layers')
-parser.add_argument('--lpa_iter', type=int, default=2, help='number of LPA iterations')
-parser.add_argument('--l2_weight', type=float, default=1e-4, help='weight of l2 regularization')
-parser.add_argument('--lpa_weight', type=float, default=2, help='weight of LP regularization')
-parser.add_argument('--dropout', type=float, default=0.2, help='dropout rate')
-parser.add_argument('--lr', type=float, default=0.1, help='learning rate')
-'''
-
-'''
-# coauthor-phy
-parser.add_argument('--dataset', type=str, default='coauthor-phy', help='which dataset to use')
-parser.add_argument('--epochs', type=int, default=200, help='the number of epochs')
-parser.add_argument('--dim', type=int, default=32, help='dimension of hidden layers')
-parser.add_argument('--gcn_layer', type=int, default=2, help='number of GCN layers')
-parser.add_argument('--lpa_iter', type=int, default=3, help='number of LPA iterations')
-parser.add_argument('--l2_weight', type=float, default=1e-4, help='weight of l2 regularization')
-parser.add_argument('--lpa_weight', type=float, default=1, help='weight of LP regularization')
-parser.add_argument('--dropout', type=float, default=0.2, help='dropout rate')
-parser.add_argument('--lr', type=float, default=0.05, help='learning rate')
-'''
-
-'''
-# random graph
-# this is only for calculating the training time
-parser.add_argument('--dataset', type=str, default='random', help='which dataset to use')
-parser.add_argument('--epochs', type=int, default=100, help='the number of epochs')
-parser.add_argument('--dim', type=int, default=16, help='dimension of hidden layers')
-parser.add_argument('--gcn_layer', type=int, default=5, help='number of GCN layers')
-parser.add_argument('--lpa_iter', type=int, default=6, help='number of LPA iterations')
-parser.add_argument('--l2_weight', type=float, default=5e-8, help='weight of l2 regularization')
-parser.add_argument('--lpa_weight', type=float, default=15, help='weight of LP regularization')
-parser.add_argument('--dropout', type=float, default=0.2, help='dropout rate')
-parser.add_argument('--lr', type=float, default=0.1, help='learning rate')
-'''
+switch_case(2)  
 
 t = time()
 args = parser.parse_args()
@@ -100,4 +86,5 @@ else:
     data = load_random(n_nodes=n_nodes, n_train=100, n_val=200, p=10/n_nodes)
 
 train(args, data)
+print(args)
 print('time used: %d s' % (time() - t))
